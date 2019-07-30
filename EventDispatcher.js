467ba923b835efda8913f3e8fb65ae41dbd45230
Javi Carrion <suavejavi@google.com>
2019-07-30 16:08:30 -0400

    Style: formated code in EventDispatcher.js

--- EventDispatcher.js
+++ EventDispatcher.js
@@ -1,18 +1,16 @@
-const Event = function (sender) {
-    this._sender = sender;
-    this.listeners = [];
-}
+const Event = function(sender) {
+	this._sender = sender;
+	this.listeners = [];
+};
 
 Event.prototype = {
+	attach: function(listener) {
+		this._listeners.push(listener);
+	},
 
-    attach: function (listener) {
-        this._listeners.push(listener)
-    },
-
-    notify: function (args) {
-        for (let i = 0; i < this._listeners.length; i += 1) {
-            this._listeners[i](this._sender, args);
-
-        }
-    }
+	notify: function(args) {
+		for (let i = 0; i < this._listeners.length; i += 1) {
+			this._listeners[i](this._sender, args);
+		}
+	},
 };
